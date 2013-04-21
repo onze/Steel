ab5bb39443925277f1e02dbf11f9a0246cd3c683
onze <onzeonline@gmail.com>
2013-04-21 13:46:32 -0400

    Implemented BTShapeStream construction from filesystem parsing.

--- Steel/include/BT/BTShapeManager.h
+++ Steel/include/BT/BTShapeManager.h
@@ -11,31 +11,37 @@
 namespace Steel
 {
     /**
-     * Handles Behavior Trees shapes.
+     * Handles behavior tree shape streams, that is BTShapeStream.
+     * Could be a manager on its own.
      */
     class BTShapeManager
     {
 
         public:
-            
+
             BTShapeManager();
             BTShapeManager(const BTShapeManager& o);
             virtual ~BTShapeManager();
             virtual BTShapeManager& operator=(const BTShapeManager& o);
             virtual bool operator==(const BTShapeManager& o) const;
-            
+
             /**
-             * Builds a shape stream out of a file system, saves it under the given streamId, 
+             * Builds a shape stream out of a file system, saves it under the given streamId,
              * sets the given stream pointer to the stream address, and returns true if all operations
-             * succeeded. Otherwise, returns false.
+             * succeeded.
+             * @param streamId the name of the root folder of the tree, from the base BT resources folder path.
+             * @param rootFile File pointing to the rot folder of the tree.
+             * @param stream pointer to the created shape stream.
+             * @return true if no error occured.
              */
             bool buildShapeStream(Ogre::String streamId, Steel::File& rootFile, Steel::BTShapeStream*& stream);
-            
-    protected:
-        // not owned
-        
-        // owned
-        BTShapeStreamMap mStreamMap;
+
+        protected:
+            // not owned
+
+            // owned
+            /// map streamIds to shapeStreams
+            BTShapeStreamMap mStreamMap;
     };
 
 }
--- Steel/src/BT/BTShapeManager.cpp
+++ Steel/src/BT/BTShapeManager.cpp
@@ -1,4 +1,5 @@
 #include <list>
+#include <stack>
 
 #include "BT/BTShapeManager.h"
 #include <BT/BTFileNode.h>
@@ -32,24 +33,49 @@ namespace Steel
         return false;
     }
 
-    bool BTShapeManager::buildShapeStream(Ogre::String streamId, Steel::File &rootFile, BTShapeStream *&streamPtr)
+    bool BTShapeManager::buildShapeStream(Ogre::String streamId,
+                                          Steel::File &rootFile,
+                                          BTShapeStream *&streamPtr)
     {
+        BTShapeStreamMap::iterator it;
+        // already built ?
+        if(mStreamMap.end()!=(it=mStreamMap.find(streamId)))
+        {
+            streamPtr=&(*it).second;
+            return true;
+        }
+
         BTShapeStream stream;
         // walk the file hierarchy depth first, using the fringe as buffer.
         std::list<BTFileNode> fringe;
+        fringe.push_front(BTFileNode(true));
         fringe.push_front(BTFileNode(rootFile));
+        unsigned int currentIndex=0;
+        std::stack<unsigned int> stack;
+
         while(fringe.size())
         {
             BTFileNode file=fringe.front();
             fringe.pop_front();
 
-            // push the type to the stream
-            stream.push_back(file.shapeTokenType());
+            if(file.isGuard())
+            {
+                // popped back to root ?
+                if(stack.size()==0)
+                    break;
+                stream[stack.top()].end=currentIndex;
+                stack.pop();
+                continue;
+            }
 
+            // push the token to the stream
+            stream.push_back( {file.shapeTokenType(),currentIndex,0});
+            
             // add childNodes to fringe for them to be processed next.
             std::vector<BTFileNode> childNodes=file.childNodes();
-            if(childNodes.size()>0)
-                fringe.insert(fringe.begin(),childNodes.begin(),childNodes.end());
+            fringe.insert(fringe.begin(),childNodes.begin(),childNodes.end());
+            stack.push(currentIndex);
+            ++currentIndex;
         }
 
         mStreamMap[streamId]=stream;
