4626151ea8d4b86b2d18cffed3cf1ddf6d7b8267
onze <onzeonline@gmail.com>
2013-07-26 00:47:53 -0400

    @minor mainly formatting

--- Steel/src/Agent.cpp
+++ Steel/src/Agent.cpp
@@ -19,41 +19,70 @@
 namespace Steel
 {
 
-    AgentId Agent::sNextId = 0;
+AgentId Agent::sNextId = 0;
 
-    Agent::Agent(Level *level) :
-        mId(Agent::getNextId()), mLevel(level)
+Agent::Agent(Level *level)
+        : mId(Agent::getNextId()), mLevel(level)
+{
+    mModelIds = std::map<ModelType, ModelId>();
+}
+
+Agent::~Agent()
+{
+    for (std::map<ModelType, ModelId>::iterator it = mModelIds.begin(); it != mModelIds.end(); ++it)
+        mLevel->modelManager(it->first)->decRef(it->second);
+    mModelIds.clear();
+}
+
+Agent::Agent(const Agent &t)
+        : mId(t.mId), mLevel(t.mLevel), mModelIds(t.mModelIds)
+{
+    for (std::map<ModelType, ModelId>::iterator it = mModelIds.begin(); it != mModelIds.end(); ++it)
     {
-        mModelIds = std::map<ModelType, ModelId>();
+        mLevel->modelManager(it->first)->at(it->second)->incRef();
     }
+}
 
-    Agent::~Agent()
+Agent &Agent::operator=(const Agent &t)
+{
+    mId = t.mId;
+    mLevel = t.mLevel;
+    mModelIds = t.mModelIds;
+    for (std::map<ModelType, ModelId>::iterator it = mModelIds.begin(); it != mModelIds.end(); ++it)
     {
-        for (std::map<ModelType, ModelId>::iterator it = mModelIds.begin(); it != mModelIds.end(); ++it)
-            mLevel->modelManager(it->first)->decRef(it->second);
-        mModelIds.clear();
+        mLevel->modelManager(it->first)->at(it->second)->incRef();
     }
+    return *this;
+}
 
-    Agent::Agent(const Agent &t) :
-        mId(t.mId), mLevel(t.mLevel), mModelIds(t.mModelIds)
+bool Agent::fromJson(Json::Value &value)
+{
+//	Debug::log("Agent<")(mId)(">::fromJson():").endl()(value.toStyledString()).endl();
+    int nModels = 0, nExpected = 0;
+    for (ModelType mt_it = (ModelType) ((int) MT_FIRST + 1); mt_it != MT_LAST; mt_it = (ModelType) ((int) mt_it + 1))
     {
-        for (std::map<ModelType, ModelId>::iterator it = mModelIds.begin(); it != mModelIds.end(); ++it)
+        Ogre::String mtName = modelTypesAsString[mt_it];
+        Json::Value mTypeValue = value[mtName];
+        // possibly no model of this type
+        if (mTypeValue.isNull())
+            continue;
+        ++nExpected;
+        ModelId modelId = (ModelId) Ogre::StringConverter::parseUnsignedLong(mTypeValue.asString());
+        if (!linkToModel(mt_it, modelId))
         {
-            mLevel->modelManager(it->first)->at(it->second)->incRef();
+            Debug::error("Agent::fromJson(): agent ")(mId);
+            Debug::error(" would not link with model<>")(mtName)("> ")(mId)(". Skipping.").endl();
+            continue;
         }
+        ++nModels;
     }
-
-    Agent &Agent::operator=(const Agent &t)
+    if (nModels != nExpected)
     {
-        mId = t.mId;
-        mLevel = t.mLevel;
-        mModelIds = t.mModelIds;
-        for (std::map<ModelType, ModelId>::iterator it = mModelIds.begin(); it != mModelIds.end(); ++it)
-        {
-            mLevel->modelManager(it->first)->at(it->second)->incRef();
-        }
-        return *this;
+        Debug::warning("Agent::fromJson(): agent ")(mId)(" linked with (")(nModels)(" models, ");
+        Debug::warning(nExpected)(" were expected. Json string:").endl()(value).endl();
     }
+    return true;
+}
 
 bool Agent::linkToModel(ModelType mType, ModelId modelId)
 {
@@ -102,122 +131,134 @@ void Agent::unlinkFromModel(ModelType mType)
     }
 }
 
-    Model *Agent::model(ModelType mType) const
+Model *Agent::model(ModelType mType) const
+{
+    ModelId id = modelId(mType);
+
+    if (id == INVALID_ID)
+        return NULL;
+
+    return mLevel->modelManager(mType)->at(id);
+}
+
+ModelId Agent::modelId(ModelType mType) const
+{
+    auto it = mModelIds.find(mType);
+    return (it == mModelIds.end() ? INVALID_ID : it->second);
+}
+
+void Agent::setSelected(bool selected)
+{
+    OgreModel *om = ogreModel();
+    if (om != NULL)
+        om->setSelected(selected);
+    PhysicsModel *pmodel = physicsModel();
+    if (NULL != pmodel)
     {
-        ModelId id = modelId(mType);
-
-        if (id == INVALID_ID)
-            return NULL;
-
-        return mLevel->modelManager(mType)->at(id);
-    }
-
-    ModelId Agent::modelId(ModelType mType) const
-    {
-        auto it = mModelIds.find(mType);
-        return (it == mModelIds.end() ? INVALID_ID : it->second);
-    }
-
-    void Agent::setSelected(bool selected)
-    {
-        OgreModel *om = ogreModel();
-        if (om != NULL)om->setSelected(selected);
-        auto pmodel=physicsModel();
-        if(NULL!=pmodel)
+        if (selected)
         {
-            if(selected)
-            {
-                pmodel->pushState();
-                pmodel->toKinematics();
-            }
-            else
-            {
-                pmodel->popState();
-            }
+            pmodel->pushState();
+            pmodel->toKinematics();
+        }
+        else
+        {
+            pmodel->popState();
         }
     }
+}
 
-    Json::Value Agent::toJson()
-    {
+Json::Value Agent::toJson()
+{
 //	Debug::log("Agent<")(mId)("> with ")(mModelIds.size())(" mTypes:").endl();
-        Json::Value root;
-        // add the agent's model ids to its json representation
-        for (std::map<ModelType, ModelId>::iterator it = mModelIds.begin(); it != mModelIds.end(); ++it)
-        {
-            ModelType mt = (*it).first;
+    Json::Value root;
+    // add the agent's model ids to its json representation
+    for (std::map<ModelType, ModelId>::iterator it = mModelIds.begin(); it != mModelIds.end(); ++it)
+    {
+        ModelType mt = (*it).first;
 //		Debug::log("model type:")(modelTypesAsString[mt]).endl();
-            ModelId mid = (*it).second;
-            root[modelTypesAsString[mt]] = StringUtils::toJson(mid);
-        }
-        return root;
+        ModelId mid = (*it).second;
+        root[modelTypesAsString[mt]] = StringUtils::toJson(mid);
     }
+    return root;
+}
 
-    Ogre::Vector3 Agent::position() const
-    {
-        auto omodel=ogreModel();
-        return NULL==omodel?Ogre::Vector3::ZERO:omodel->position();
-    }
+Ogre::Vector3 Agent::position() const
+{
+    auto omodel = ogreModel();
+    return NULL == omodel ? Ogre::Vector3::ZERO : omodel->position();
+}
 
-    Ogre::Quaternion Agent::rotation() const
-    {
-        auto omodel=ogreModel();
-        return NULL==omodel?Ogre::Quaternion::ZERO:omodel->rotation();
-    }
+Ogre::Quaternion Agent::rotation() const
+{
+    auto omodel = ogreModel();
+    return NULL == omodel ? Ogre::Quaternion::ZERO : omodel->rotation();
+}
 
-    Ogre::Vector3 Agent::scale() const
-    {
-        auto omodel=ogreModel();
-        return NULL==omodel?Ogre::Vector3::ZERO:omodel->scale();
-    }
+Ogre::Vector3 Agent::scale() const
+{
+    auto omodel = ogreModel();
+    return NULL == omodel ? Ogre::Vector3::ZERO : omodel->scale();
+}
 
-    void Agent::move(const Ogre::Vector3 &dpos)
-    {
-        auto omodel=ogreModel();
-        if(NULL!=omodel)omodel->move(dpos);
-        auto pmodel=physicsModel();
-        if(NULL!=pmodel)pmodel->move(dpos);
-    }
+void Agent::move(const Ogre::Vector3 &dpos)
+{
+    auto omodel = ogreModel();
+    if (NULL != omodel)
+        omodel->move(dpos);
+    auto pmodel = physicsModel();
+    if (NULL != pmodel)
+        pmodel->move(dpos);
+}
 
-    void Agent::rotate(const Ogre::Vector3& rot)
-    {
-        auto omodel=ogreModel();
-        if(NULL!=omodel)omodel->rotate(rot);
-    }
+void Agent::rotate(const Ogre::Vector3& rot)
+{
+    auto omodel = ogreModel();
+    if (NULL != omodel)
+        omodel->rotate(rot);
+}
 
-    void Agent::rotate(const Ogre::Quaternion &q)
-    {
-        auto omodel=ogreModel();
-        if(NULL!=omodel)omodel->rotate(q);
-    }
+void Agent::rotate(const Ogre::Quaternion &q)
+{
+    auto omodel = ogreModel();
+    if (NULL != omodel)
+        omodel->rotate(q);
+}
 
-    void Agent::rescale(const Ogre::Vector3 &sca)
-    {
-        auto omodel=ogreModel();
-        if(NULL!=omodel)omodel->rescale(sca);
-        auto pmodel=physicsModel();
-        if(NULL!=pmodel)pmodel->rescale(sca);
-    }
+void Agent::rescale(const Ogre::Vector3 &sca)
+{
+    auto omodel = ogreModel();
+    if (NULL != omodel)
+        omodel->rescale(sca);
+    auto pmodel = physicsModel();
+    if (NULL != pmodel)
+        pmodel->rescale(sca);
+}
 
-    void Agent::setPosition(const Ogre::Vector3 &pos)
-    {
-        auto omodel=ogreModel();
-        if(NULL!=omodel)omodel->setPosition(pos);
-        auto pmodel=physicsModel();
-        if(NULL!=pmodel)pmodel->setPosition(pos);
-    }
+void Agent::setPosition(const Ogre::Vector3 &pos)
+{
+    auto omodel = ogreModel();
+    if (NULL != omodel)
+        omodel->setPosition(pos);
+    auto pmodel = physicsModel();
+    if (NULL != pmodel)
+        pmodel->setPosition(pos);
+}
 
-    void Agent::setRotation(const Ogre::Quaternion &rot)
-    {
-        auto omodel=ogreModel();
-        if(NULL!=omodel)omodel->setRotation(rot);
-    }
+void Agent::setRotation(const Ogre::Quaternion &rot)
+{
+    auto omodel = ogreModel();
+    if (NULL != omodel)
+        omodel->setRotation(rot);
+}
 
-    void Agent::setScale(const Ogre::Vector3 &sca)
-    {
-        auto omodel=ogreModel();
-        if(NULL!=omodel)omodel->setScale(sca);
-        auto pmodel=physicsModel();
-        if(NULL!=pmodel)pmodel->setScale(sca);
-    }
+void Agent::setScale(const Ogre::Vector3 &sca)
+{
+    auto omodel = ogreModel();
+    if (NULL != omodel)
+        omodel->setScale(sca);
+    auto pmodel = physicsModel();
+    if (NULL != pmodel)
+        pmodel->setScale(sca);
+}
 }
 // kate: indent-mode cstyle; indent-width 4; replace-tabs on; 
