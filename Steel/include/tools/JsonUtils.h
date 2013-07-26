b84a3a787a7ad9199e48bab11c40b33c4a122a91
onze <onzeonline@gmail.com>
2013-07-26 00:49:51 -0400

    First commit for  JsonUtils.{h, cpp}

---
+++ Steel/include/tools/JsonUtils.h
@@ -0,0 +1,19 @@
+#ifndef STEEL_JSONUTILS_H
+#define STEEL_JSONUTILS_H
+
+#include <json/json.h>
+#include <OgreString.h>
+
+namespace Steel
+{
+class JsonUtils
+{
+public:
+    static void updateObjectWithOverrides(Json::Value const &src, Json::Value &dst);
+private:
+    /// Copies src keys to dst keys.
+    static void updateObject(Json::Value const &src, Json::Value &dst, bool overrideDuplicates, bool warnOnDuplicates);
+};
+}
+#endif
+// kate: indent-mode cstyle; indent-width 4; replace-tabs on;
---
+++ Steel/src/tools/JsonUtils.cpp
@@ -0,0 +1,31 @@
+#include "tools/JsonUtils.h"
+#include "Debug.h"
+
+namespace Steel
+{
+
+void JsonUtils::updateObjectWithOverrides(Json::Value const &src, Json::Value &dst)
+{
+    JsonUtils::updateObject(src, dst, false, true);
+}
+
+void JsonUtils::updateObject(Json::Value const &src, Json::Value &dst, bool const overrideDuplicates,
+        bool const warnOnDuplicates)
+{
+    Ogre::String intro = "JsonUtils::updateObject";
+    auto names = src.getMemberNames();
+    bool detectDuplicates = overrideDuplicates || warnOnDuplicates;
+    for (auto it = names.begin(); it != names.end(); ++it)
+    {
+        if (detectDuplicates && dst.isMember(*it))
+        {
+            if (warnOnDuplicates)
+                Debug::warning(intro)("duplicate key \"")(*it)("\"").endl();
+            if (!overrideDuplicates)
+                continue;
+        }
+        dst[*it] = src[*it];
+    }
+}
+
+}
