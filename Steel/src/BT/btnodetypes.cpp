3d4649c0bbad1a43e4633a2f6381c8d948eae3d9
onze <onzeonline@gmail.com>
2013-08-13 19:21:35 -0400

    Created BTStateAsString for debug purposes

--- Steel/src/BT/btnodetypes.cpp
+++ Steel/src/BT/btnodetypes.cpp
@@ -22,7 +22,21 @@ namespace Steel
     std::vector<Ogre::String> BTShapeTokenTypeAsString = std::vector<Ogre::String> (
                 BTShapeTokenTypeAsString__data,
                 BTShapeTokenTypeAsString__data+sizeof(BTShapeTokenTypeAsString__data)/sizeof(Ogre::String)
-            );
+    );
+    
+    Ogre::String BTStateAsString__data[] =
+    {
+        "READY",
+        "RUNNING",
+        "SUCCESS",
+        "FAILURE",
+        "SKIP_TO"
+    };
+    
+    std::vector<Ogre::String> BTStateAsString = std::vector<Ogre::String> (
+        BTStateAsString__data,
+        BTStateAsString__data+sizeof(BTStateAsString__data)/sizeof(Ogre::String)
+    );
 
 }
 // kate: indent-mode cstyle; indent-width 4; replace-tabs on; 
