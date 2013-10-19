4af2e39d088c38acd0116e54d0373d2532f63a9f
onze <onzeonline@gmail.com>
2013-10-19 14:03:49 +0000

    Updated utests_BTShapeStream.cpp accoring to data

--- Steel/src/tests/utests_BTShapeStream.cpp
+++ Steel/src/tests/utests_BTShapeStream.cpp
@@ -19,18 +19,20 @@ namespace Steel
      */
     bool test_BTShapeStream()
     {
-        Ogre::String intro="test_BTShapeStream(): ",abortMsg="Aborting unit test.";
+        Ogre::String intro = "test_BTShapeStream(): ", abortMsg = "Aborting unit test.";
 
         BTShapeManager shapeMan;
-        Ogre::String streamId="utests/shapes/A - sequence";
+        Ogre::String streamId = "utests/shapes/A - sequence";
         File rootFile("/media/a0/cpp/1210/usmb/data/raw_resources/BT/utests/shapes/A - sequence");
-        BTShapeStream *stream=nullptr;
+        BTShapeStream *stream = nullptr;
+
         if(!rootFile.exists())
         {
             Debug::warning(intro)(rootFile)(" not found. ")(abortMsg).endl();
             return false;
         }
-        if(!shapeMan.buildShapeStream(streamId,rootFile,stream))
+
+        if(!shapeMan.buildShapeStream(streamId, rootFile, stream))
         {
             Debug::error(intro)("Could not create shape stream, see above for details.")(abortMsg).endl();
             return false;
@@ -38,26 +40,26 @@ namespace Steel
 
         BTShapeToken groundTruth_data[] =
         {
-            {BTSequenceToken,0,7,BTFileNode(rootFile).descriptor()},                                                  //A
-            {BTSequenceToken,1,4,BTFileNode(rootFile.subfile("B - sequence")).descriptor()},                          //+-B
-            {BTFinderToken,2,3,BTFileNode(rootFile.subfile("B - sequence").subfile("C - finder")).descriptor()},      //| +-C
-            {BTNavigatorToken,3,4,BTFileNode(rootFile.subfile("B - sequence").subfile("D - navigator")).descriptor()},//| |-D
-            {BTSequenceToken,4,7,BTFileNode(rootFile.subfile("E - sequence")).descriptor()},                          //+-E
-            {BTFinderToken,5,6,BTFileNode(rootFile.subfile("E - sequence").subfile("F - finder")).descriptor()},      //  +-F
-            {BTNavigatorToken,6,7,BTFileNode(rootFile.subfile("E - sequence").subfile("G - navigator")).descriptor()},//  |-G
+            {BTSequenceToken, 0, 7, BTFileNode(rootFile).descriptor()},                                               //A
+            {BTSequenceToken, 1, 4, BTFileNode(rootFile.subfile("B - sequence")).descriptor()},                       //+-B
+            {BTFinderToken, 2, 3, BTFileNode(rootFile.subfile("B - sequence").subfile("C - finder")).descriptor()},   //| +-C
+            {BTNavigatorToken, 3, 4, BTFileNode(rootFile.subfile("B - sequence").subfile("D - navigator")).descriptor()}, //| |-D
+            {BTSequenceToken, 4, 7, BTFileNode(rootFile.subfile("E - sequence")).descriptor()},                       //+-E
+            {BTFinderToken, 5, 6, BTFileNode(rootFile.subfile("E - sequence").subfile("F - finder")).descriptor()},   //  +-F
+            {BTNavigatorToken, 6, 7, BTFileNode(rootFile.subfile("E - sequence").subfile("G - navigator")).descriptor()}, //  |-G
         };
         BTShapeStream groundTruth;
         groundTruth.mName = streamId;
         groundTruth.mData = BTShapeStreamData(groundTruth_data,
-                                              groundTruth_data+sizeof(groundTruth_data)/sizeof(BTShapeToken));
+                                              groundTruth_data + sizeof(groundTruth_data) / sizeof(BTShapeToken));
 //         BTShapeStream groundTruth;
 //         groundTruth.push_back({BTSequenceToken,0,7});
-        std::pair<BTShapeStreamData::iterator,BTShapeStreamData::iterator> mismatch;
+        std::pair<BTShapeStreamData::iterator, BTShapeStreamData::iterator> mismatch;
 
-        // using default comparison:
-        mismatch = std::mismatch (stream->mData.begin(), stream->mData.end(), groundTruth.mData.begin());
+        // using default comparison
+        mismatch = std::mismatch(stream->mData.begin(), stream->mData.end(), groundTruth.mData.begin());
 
-        if(mismatch.first!=stream->mData.end())
+        if(mismatch.first != stream->mData.end())
         {
             Debug::error(intro)("BTShapeStream mismatch with groundTruth:").endl();
             Debug::error("streamId: ")(streamId).endl();
