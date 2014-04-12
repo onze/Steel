4f18a97205e078d8235ad16adccc684ff33ce4a7
onze <onzeonline@gmail.com>
2014-04-12 15:31:26 -0400

    Added OgreUtils::loadTextureFromFile and OgreUtils::pixelColor

--- Steel/include/tools/OgreUtils.h
+++ Steel/include/tools/OgreUtils.h
@@ -7,26 +7,39 @@
 namespace Ogre
 {
     class SceneNode;
+    class ColourValue;
 }
 
 namespace Steel
 {
     class OgreUtils
     {
-        public:
-            ///see http://www.ogre3d.org/forums/viewtopic.php?p=445350#p445350
-            static void destroySceneNode(Ogre::SceneNode* node);
+    public:
+        /// See http://www.ogre3d.org/forums/viewtopic.php?p=445350#p445350
+        static void destroySceneNode(Ogre::SceneNode *node);
 
-            ///see http://www.ogre3d.org/forums/viewtopic.php?p=445350#p445350
-            static void destroyAllAttachedMovableObjects(Ogre::SceneNode* node);
+        /// See http://www.ogre3d.org/forums/viewtopic.php?p=445350#p445350
+        static void destroyAllAttachedMovableObjects(Ogre::SceneNode *node);
 
-            /// Returns the mean of the given class. Class must implement operator+(T const &) and operator/(float).
-            template<class T>
-            static T mean(std::vector<T> const &v);
+        /// Returns the mean of the given class. Class must implement operator+(T const &) and operator/(float).
+        template<class T>
+        static T mean(std::vector<T> const &v);
 
-            /// prints resource Groups and their content
-            static void resourceGroupsInfos();
+        /// Prints resource Groups and their content
+        static void resourceGroupsInfos();
+
+        /// Loads the given file as a 2d texture and returns a pointer to it. Requires the Ogre resource name and group to register it in.
+        static Ogre::TexturePtr loadTextureFromFile(File const &file, Ogre::String const &name, Ogre::String const &group);
+
+        /// Returns the pixel color of the given texture at the given location. White by default.
+        static Ogre::ColourValue pixelColor(Ogre::TexturePtr const &tex, unsigned const x, unsigned const y);
     };
+}
+
+namespace std
+{
+    /// Ogre::ColourValue comparator
+    bool operator<(Ogre::ColourValue const &c0, Ogre::ColourValue const &c1);
 }
 #endif
 // kate: indent-mode cstyle; indent-width 4; replace-tabs on; 
--- Steel/src/tools/OgreUtils.cpp
+++ Steel/src/tools/OgreUtils.cpp
@@ -2,8 +2,11 @@
 
 #include <OgreSceneManager.h>
 #include <OgreSceneNode.h>
+#include <OgreColourValue.h>
+#include <OgrePixelFormat.h>
+#include <OgreImage.h>
 
-#include <Debug.h>
+#include "Debug.h"
 
 namespace Steel
 {
@@ -22,7 +25,7 @@ namespace Steel
 
         while(itChild.hasMoreElements())
         {
-            Ogre::SceneNode* pChildNode = static_cast<Ogre::SceneNode*>(itChild.getNext());
+            Ogre::SceneNode *pChildNode = static_cast<Ogre::SceneNode *>(itChild.getNext());
             OgreUtils::destroyAllAttachedMovableObjects(pChildNode);
         }
     }
@@ -31,6 +34,7 @@ namespace Steel
     {
         if(!node)
             return;
+
         OgreUtils::destroyAllAttachedMovableObjects(node);
         node->removeAndDestroyAllChildren();
         node->getCreator()->destroySceneNode(node);
@@ -39,14 +43,18 @@ namespace Steel
     template<class T>
     T OgreUtils::mean(std::vector<T> const &v)
     {
-        if(v.size()==0)
+        if(v.size() == 0)
             return T();
-        if(v.size()==1)
+
+        if(v.size() == 1)
             return v[0];
-        T mean=v[0];
-        for(auto it=v.begin()+1; it!=v.end(); ++it)
-            mean+=*it;
-        return mean/float(v.size());
+
+        T mean = v[0];
+
+        for(auto it = v.begin() + 1; it != v.end(); ++it)
+            mean += *it;
+
+        return mean / float(v.size());
     }
     // template definition
     template Ogre::Vector3 OgreUtils::mean<Ogre::Vector3>(std::vector<Ogre::Vector3> const &);
@@ -54,8 +62,9 @@ namespace Steel
     void OgreUtils::resourceGroupsInfos()
     {
         Debug::log("== Resources locations ==").indent().endl();
-        auto resGroupNames=Ogre::ResourceGroupManager::getSingleton().getResourceGroups();
-        for(auto it=resGroupNames.begin(); it!=resGroupNames.end(); ++it)
+        auto resGroupNames = Ogre::ResourceGroupManager::getSingleton().getResourceGroups();
+
+        for(auto it = resGroupNames.begin(); it != resGroupNames.end(); ++it)
         {
             Debug::log(*it)(":").endl();
             Debug::log("\tlocations:");
@@ -63,8 +72,78 @@ namespace Steel
             Debug::log("\tfiles:");
             Debug::log(Ogre::ResourceGroupManager::getSingleton().getResourceDeclarationList(*it)).endl();
         }
+
         Debug::log.unIndent().endl();
     }
 
+    Ogre::TexturePtr OgreUtils::loadTextureFromFile(File const &file, Ogre::String const &name, Ogre::String const &group)
+    {
+        Ogre::TexturePtr texPtr;
+
+        if(file.exists())
+        {
+            Ogre::Image img = Ogre::Image().load(file.fullPath(), group);
+            texPtr = Ogre::TextureManager::getSingleton().loadImage(name, group, img);
+        }
+        else
+        {
+            Debug::error("OgreUtils::loadTextureFromFile(): ")("file not found: ")(file).endl();
+            texPtr.setNull();
+        }
+
+        return texPtr;
+    }
+
+    Ogre::ColourValue OgreUtils::pixelColor(Ogre::TexturePtr const &tex, unsigned const x, unsigned const y)
+    {
+        Ogre::ColourValue color(Ogre::ColourValue::White);
+
+        if(tex.isNull())
+            return color;
+
+        /// segfaults
+        size_t index = y * tex->getWidth() + x;
+
+        if(index >= tex->getWidth()*tex->getHeight())
+            return color;
+
+        Ogre::PixelBox box(1, 1, 1, Ogre::PixelFormat::PF_A8R8G8B8);
+        Ogre::HardwarePixelBufferSharedPtr buffer = tex->getBuffer();
+        // windows, see buffer.blitToMemory ?
+        const Ogre::PixelBox &pixBox  = buffer->lock(box, Ogre::HardwareBuffer::HBL_NO_OVERWRITE);
+
+        Ogre::uint8 *pDest = static_cast<Ogre::uint8 *>(pixBox.data);
+        size_t elemSize = Ogre::PixelUtil::getNumElemBytes(pixBox.format);
+        Ogre::uint8 *offset = pDest + index * elemSize;
+
+        switch(box.format)
+        {
+            case Ogre::PixelFormat::PF_A8R8G8B8:
+                color.a = float(*(offset++)) / 255.f;
+                color.r = float(*(offset++)) / 255.f;
+                color.g = float(*(offset++)) / 255.f;
+                color.b = float(*(offset++)) / 255.f;
+                break;
+
+            case Ogre::PixelFormat::PF_B8G8R8A8:
+            default:
+                color.b = float(*(offset++)) / 255.f;
+                color.g = float(*(offset++)) / 255.f;
+                color.r = float(*(offset++)) / 255.f;
+                color.a = float(*(offset++)) / 255.f;
+                break;
+        }
+
+        buffer->unlock();
+
+        return color;
+    }
+}
+namespace std
+{
+    bool operator<(Ogre::ColourValue const &c0, Ogre::ColourValue const &c1)
+    {
+        return c0.getAsRGBA() < c1.getAsRGBA();
+    }
 }
 // kate: indent-mode cstyle; indent-width 4; replace-tabs on; 
