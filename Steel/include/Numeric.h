4fbbc3099b084486e8b9d426c1a0d6e7bfd672d2
onze <onzeonline@gmail.com>
2014-05-09 00:47:41 -0400

    Added min and max to Numeric

--- Steel/include/Numeric.h
+++ Steel/include/Numeric.h
@@ -11,6 +11,19 @@ namespace Steel
         {
             return value < min ? min : (value > max ? max : value);
         }
+        
+        template<typename T>
+        static T min(T const& x, T const& y)
+        {
+            return x < y ? x : y;
+        }
+        
+        template<typename T>
+        static T max(T const& x, T const& y)
+        {
+            return x < y ? y : x;
+        }
+        
     };
 }
 
