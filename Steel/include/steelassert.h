b851e8530bc2971ee3782ff6addc2c607e562964
onze <onzeonline@gmail.com>
2014-05-12 23:00:39 -0400

    Updated STEEL_WRONG_CODE_PATH

--- Steel/include/steelassert.h
+++ Steel/include/steelassert.h
@@ -6,6 +6,6 @@
 
 #define STEEL_ASSERT(EXPR, ...) if(!(EXPR)){Steel::Debug::log("Assert in ", __FILE__,"@", __LINE__, " on ").quotes(#EXPR)("==", bool(EXPR), ": ")(__VA_ARGS__).endl().breakHere();}
 
-#define STEEL_WRONG_CODE_PATH(...) Debug::error(STEEL_FUNC_INTRO,"wrong code path. ")(__VA_ARGS__).endl().breakHere()
+#define STEEL_WRONG_CODE_PATH(...) Debug::error("wrong code path. ")(__VA_ARGS__).endl().breakHere()
 
 #endif // STEEL_ASSERT_H
